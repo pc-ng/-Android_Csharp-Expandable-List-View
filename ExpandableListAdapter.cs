a6e479920c7ffbbb75f966ac0c3910db5bb5df9a
pcngnotgood <38524186+pcngnotgood@users.noreply.github.com>
2018-05-30 00:34:17 +0800

    Add files via upload

---
+++ ExpandableListAdapter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Expandable_List_View
+{
+    class ExpandableListAdapter : BaseExpandableListAdapter
+    {
+        Dictionary<string, List<string>> groupItems;
+        List<string> groupKeys;
+        Activity activity;
+
+        public ExpandableListAdapter(Activity activity, Dictionary<string, List<string>> groupItems)
+        {
+            this.groupItems = groupItems;
+            this.activity = activity;
+            this.groupKeys = groupItems.Keys.ToList();
+        }
+
+        public override int GroupCount
+        {
+            get{ return groupItems.Count; }
+        }
+
+        public override bool HasStableIds
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Handle the child items
+        /// </summary>
+        /// <param name="groupPosition"></param>
+        /// <param name="childPosition"></param>
+        /// <returns></returns>
+        public override Java.Lang.Object GetChild(int groupPosition, int childPosition)
+        {
+            return groupItems[groupKeys[groupPosition]][childPosition];
+        }
+
+        public override long GetChildId(int groupPosition, int childPosition)
+        {
+            return childPosition;
+        }
+
+        public override int GetChildrenCount(int groupPosition)
+        {
+            return groupItems[groupKeys[groupPosition]].Count;
+        }
+
+        public override View GetChildView(int groupPosition, int childPosition, bool isLastChild, View convertView, ViewGroup parent)
+        {
+            var item = groupItems[groupKeys[groupPosition]][childPosition];
+
+            if (convertView == null)
+                convertView = activity.LayoutInflater.Inflate(Resource.Layout.ChildList, null);
+
+            var textBox = convertView.FindViewById<TextView>(Resource.Id.txtSmall);
+            textBox.SetText(item, TextView.BufferType.Normal);
+
+            return convertView;
+        }
+
+        /// <summary>
+        /// Handle the group items
+        /// </summary>
+        /// <param name="groupPosition"></param>
+        /// <returns></returns>
+        public override Java.Lang.Object GetGroup(int groupPosition)
+        {
+            return groupKeys[groupPosition];
+        }
+
+        public override long GetGroupId(int groupPosition)
+        {
+            return groupPosition;
+        }
+
+        public override View GetGroupView(int groupPosition, bool isExpanded, View convertView, ViewGroup parent)
+        {
+            var item = groupKeys[groupPosition];
+
+            if (convertView == null)
+                convertView = activity.LayoutInflater.Inflate(Resource.Layout.GroupList, null);
+
+            var textBox = convertView.FindViewById<TextView>(Resource.Id.txtLarge);
+            textBox.SetText(item, TextView.BufferType.Normal);
+
+            return convertView;
+        }
+
+        public override bool IsChildSelectable(int groupPosition, int childPosition)
+        {
+            return true;
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace Expandable_List_View
+{
+    [Activity(Label = "Expandable_List_View", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ExpandableListView expandableList;
+        Dictionary<string, List<string>> groupItems = new Dictionary<string, List<string>>();
+        List<string> lstKeys = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            expandableList = FindViewById<ExpandableListView> (Resource.Id.expandableListView);
+
+            CreateExpendableListData();
+            expandableList.SetAdapter(new ExpandableListAdapter(this, groupItems));
+            expandableList.ChildClick += ExpandableList_ChildClick;
+        }
+
+        private void ExpandableList_ChildClick(object sender, ExpandableListView.ChildClickEventArgs e)
+        {
+            var itmGroup = lstKeys[e.GroupPosition];
+            var itmChild = groupItems[itmGroup][e.ChildPosition];
+
+            Toast.MakeText(this, string.Format("Group {0}: child {1}", itmGroup, itmChild), ToastLength.Long).Show();
+        }
+
+        void CreateExpendableListData()
+        {
+            for (int iGroup = 1; iGroup <= 3; iGroup++)
+            {
+                var lstChild = new List<string>();
+                for (int iChild = 1; iChild <= 3; iChild++)
+                {
+                    lstChild.Add(string.Format("Group {0} Child {1}", iGroup, iChild));
+                }
+                groupItems.Add(string.Format("Group {0}", iGroup), lstChild);
+            }
+            lstKeys = new List<string>(groupItems.Keys);
+        }
+    }
+}
+
